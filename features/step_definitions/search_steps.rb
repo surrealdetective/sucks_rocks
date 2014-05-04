cbd95d8899c0d13189d47f7aef1c064fc0e7ec82
surrealdetective <thefifth@gmail.com>
2014-05-04 18:25:24 -0400

    define api for determining score

---
+++ features/step_definitions/search_steps.rb
@@ -0,0 +1,12 @@
+When(/^I search for microsoft$/) do
+  @score = {}
+  @score[term] = RockScore.for_term(term)
+end
+
+When(/^I search for apple$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^apple should ahve a higher score then microsoft$/) do
+  pending # express the regexp above with the code you wish you had
+end
